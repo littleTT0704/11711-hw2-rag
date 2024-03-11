c0754d0f991d51f471a5420b428134d32645039b
Jiatai Li <jiatail@andrew.cmu.edu>
2024-03-10 21:57:14 -0400

    feat: clean up department; enable chunking

--- data/Courses/soc/soc.py
+++ data/Courses/soc/soc.py
@@ -33,7 +33,12 @@ def read_soc(soc_file: str, output_file: str):
     dep = None
     while i < len(df) - 1:
         row = df.iloc[i]
-        if not pd.isna(row["course"]) and pd.isna(row["lec_sec"]):
+        if (
+            not pd.isna(row["course"])
+            and pd.isna(row["title"])
+            and pd.isna(row["units"])
+            and pd.isna(row["lec_sec"])
+        ):
             if dep:
                 dep["courses"] = courses
                 res.append(dep)
@@ -52,15 +57,24 @@ def read_soc(soc_file: str, output_file: str):
                 row = df.iloc[j]
                 if j > i and not pd.isna(row["course"]):
                     break
-                section = dict()
-                section["lec_sec"] = row["lec_sec"]
-                section["days"] = row["days"]
-                section["begin"] = row["begin"]
-                section["end"] = row["end"]
-                section["room"] = row["room"]
-                section["location"] = row["location"]
-                section["instructor"] = row["instructor"]
-                sections.append(section)
+                if not (
+                    pd.isna(row["lec_sec"])
+                    and pd.isna(row["days"])
+                    and pd.isna(row["begin"])
+                    and pd.isna(row["end"])
+                    and pd.isna(row["room"])
+                    and pd.isna(row["location"])
+                    and pd.isna(row["instructor"])
+                ):
+                    section = dict()
+                    section["lec_sec"] = row["lec_sec"]
+                    section["days"] = row["days"]
+                    section["begin"] = row["begin"]
+                    section["end"] = row["end"]
+                    section["room"] = row["room"]
+                    section["location"] = row["location"]
+                    section["instructor"] = row["instructor"]
+                    sections.append(section)
 
             course["sections"] = sections
             courses.append(course)
@@ -68,8 +82,29 @@ def read_soc(soc_file: str, output_file: str):
             i = j
 
     final = {"semester": semester, "schedule": res}
-    with open(output_file, "w") as f:
+
+    with open(f"{output_file}.json", "w") as f:
         json.dump(final, f, indent=4)
+
+    with open(f"{output_file}.txt", "w") as f:
+        for d in res:
+            department = d["department"]
+            for c in d["courses"]:
+                t = [f"Semester: {semester}", f"Department: {department}"]
+                t.append(f"Course: {c['number'][:2]}-{c['number'][2:]}")
+                t.append(f"Title: {c['name']}")
+                t.append(f"Units: {c['units']}")
+                t.append("Sections")
+                for s in c["sections"]:
+                    t.append(f"Lec/Sec: {s['lec_sec']}")
+                    t.append(f"Days: {s['days']}")
+                    t.append(f"Begin: {s['begin']}")
+                    t.append(f"End: {s['end']}")
+                    t.append(f"Room: {s['room']}")
+                    t.append(f"Location: {s['location']}")
+                    t.append(f"Instructor: {s['instructor']}")
+                    t.append("")
+                f.write("\n".join(t) + "\n\n")
 
 
 if __name__ == "__main__":
@@ -80,4 +115,4 @@ if __name__ == "__main__":
         "sched_layout_summer_2.html",
     ]:
         print(soc)
-        read_soc(soc, soc.replace("html", "json"))
+        read_soc(soc, soc.replace(".html", ""))
