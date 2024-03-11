807463fc8262ba21e7a27b1445181e786ce50016
Jiatai Li <jiatail@andrew.cmu.edu>
2024-03-11 17:09:16 -0400

    feat: chunking calendar data

--- data/Courses/calendar/cal.py
+++ data/Courses/calendar/cal.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 
 def read_calendar(calendar_file: str, output_file: str):
@@ -25,62 +26,64 @@ def read_calendar(calendar_file: str, output_file: str):
             f //= 10
         return f"There are {total} work days in {section} ({m} Mondays, {t} Tuesdays, {w} Wednesdays, {th} Thursdays, {f} Fridays). {tail}"
 
-    with open(output_file, "w") as f:
-        f.write(calendar_file.replace("-list-view.xlsx", "").replace("-", " ") + "\n\n")
-        i = 0
-        while i < len(df):
-            row = df.iloc[i]
-            # empty row
-            if pd.isna(row).all():
-                f.write("\n")
-                i += 1
+    l = []
+    calendar_name = calendar_file.replace("-list-view.xlsx", "").replace("-", " ")
+    i = 0
+    while i < len(df):
+        row = df.iloc[i]
+        # empty row
+        if pd.isna(row).all():
+            i += 1
 
-            # semester header
-            elif not pd.isna(row["date"]) and pd.isna(row["day"]):
-                # semester name
-                f.write(row["date"] + "\n")
-                name = row["date"].split(" (")[1].split(")")[0]
+        # semester header
+        elif not pd.isna(row["date"]) and pd.isna(row["day"]):
+            # semester name
+            semester_name = row["date"]
+            name = row["date"].split(" (")[1].split(")")[0]
 
-                # day counts
-                j = i
-                while not pd.isna(df.iloc[j]["event"]):
-                    f.write(
-                        day_count(df.iloc[j]["event"], name) + "\n",
-                    )
-                    j += 1
+            # day counts
+            j = i
+            while not pd.isna(df.iloc[j]["event"]):
+                s = f"{calendar_name}\n{semester_name}\n{day_count(df.iloc[j]['event'], name)}\n"
+                l.append(re.sub("(\*+)", r" (\1)", s))
+                j += 1
 
-                # skip to content
-                i = j + 2
-                header = False
+            # skip to content
+            i = j + 2
+            header = False
 
-            # notes in the end
-            elif row["day"] == "Notes:":
-                break
+        # notes in the end
+        elif row["day"] == "Notes:":
+            break
 
-            # normal item
+        # normal item
+        else:
+            event = row["event"].replace("  ", " ")
+            date = row["date"].strftime("%Y-%m-%d (%A)")
+            if pd.isna(row["date_to"]):
+                s = f"{calendar_name}\n{semester_name}\n{date}: {event}\n"
+                l.append(re.sub("(\*+)", r" (\1)", s))
             else:
-                event = row["event"].replace("  ", " ")
-                date = row["date"].strftime("%Y-%m-%d (%A)")
-                if pd.isna(row["date_to"]):
-                    f.write(date + ": " + event + "\n")
-                else:
-                    date_to = row["date_to"].strftime("%Y-%m-%d (%A)")
-                    f.write(date + " to " + date_to + ": " + event + "\n")
-                i += 1
+                date_to = row["date_to"].strftime("%Y-%m-%d (%A)")
+                s = f"{calendar_name}\n{semester_name}\n{date} to {date_to}: {event}\n"
+                l.append(re.sub("(\*+)", r" (\1)", s))
+            i += 1
 
-        f.write("Footnotes: \n")
+        footnotes = {}
         for j in range(i + 1, len(df)):
             row = df.iloc[j]
-            if pd.isna(row["event"]):
-                f.write("\n")
-            else:
+            if not (pd.isna(row["event"]) or pd.isna(row["day"])):
                 marker = row["day"]
                 event = row["event"].replace("\n", "")
-                if pd.isna(marker):
-                    prefix = ""
-                else:
-                    prefix = f"({marker}): "
-                f.write(prefix + event + "\n")
+                footnotes[f" ({marker})"] = event
+
+    with open(output_file, "w") as f:
+        for s in l:
+            for marker, event in footnotes.items():
+                if marker in s:
+                    s = s.replace(marker, "") + event + "\n"
+
+            f.write(s + "\n\n")
 
 
 if __name__ == "__main__":
