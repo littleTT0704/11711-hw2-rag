8f4486c6d1050a10c67ab4f0373fcf1013fad839
Jiatai Li <jiatail@andrew.cmu.edu>
2024-03-13 19:28:59 -0400

    feat: finalize prompt template; add few_shot_pipeline

--- models.py
+++ models.py
@@ -1,40 +1,41 @@
 from haystack import Pipeline
 from haystack.schema import Document
 from haystack.document_stores import InMemoryDocumentStore
-from haystack.nodes import (
-    BM25Retriever,
-    SentenceTransformersRanker,
-    EmbeddingRetriever,
-    PromptModel,
-    AnswerParser,
-)
+from haystack.nodes import BM25Retriever, SentenceTransformersRanker, EmbeddingRetriever
 from haystack.nodes.prompt import PromptNode
-from haystack.nodes.prompt.prompt_template import PromptTemplate
-from fastrag.prompters.invocation_layers.llama_cpp import LlamaCPPInvocationLayer
+
+# from fastrag.prompters.invocation_layers.llama_cpp import LlamaCPPInvocationLayer
 import torch
 import os
 from typing import List
 
+default_prompt = """Answer the question using the provided context. Please answer as concisely as possible.
+{meta['few_shot_example']}
 
-def baseline(documents: List[Document], use_gpu: bool = False) -> Pipeline:
+
+Context: {join(documents, delimiter=new_line)}
+
+Question: {query}
+
+Answer: """
+
+
+def baseline(
+    documents: List[Document], use_gpu: bool = False, top_k: int = 1
+) -> Pipeline:
     document_store = InMemoryDocumentStore(use_gpu=use_gpu, use_bm25=True)
     document_store.write_documents(documents)
     retriever = BM25Retriever(document_store=document_store, top_k=100)
 
     reranker = SentenceTransformersRanker(
         model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2",
-        top_k=1,
+        top_k=top_k,
         use_gpu=use_gpu,
     )
 
-    answer_parser = AnswerParser()  # pattern=r"\s+(.*)")
-    lfqa_prompt = PromptTemplate(
-        prompt="Answer the question using the provided context.\n\nContext: {join(documents)}\n\nQuestion: {query}\n\nAnswer (a few words or a phrase):",
-        output_parser=answer_parser,
-    )
     prompter = PromptNode(
         model_name_or_path="MBZUAI/LaMini-Flan-T5-783M",
-        default_prompt_template=lfqa_prompt,
+        default_prompt_template=default_prompt,
         model_kwargs={"model_max_length": 2048, "torch_dtype": torch.bfloat16},
         use_gpu=use_gpu,
     )
@@ -46,7 +47,9 @@ def baseline(documents: List[Document], use_gpu: bool = False) -> Pipeline:
     return p
 
 
-def embed_meta(documents: List[Document], use_gpu: bool = False) -> Pipeline:
+def embed_meta(
+    documents: List[Document], use_gpu: bool = False, top_k: int = 1
+) -> Pipeline:
     document_store = InMemoryDocumentStore(use_gpu=use_gpu)
     document_store.write_documents(documents)
 
@@ -62,18 +65,13 @@ def embed_meta(documents: List[Document], use_gpu: bool = False) -> Pipeline:
 
     reranker = SentenceTransformersRanker(
         model_name_or_path="cross-encoder/qnli-distilroberta-base",
-        top_k=1,
+        top_k=top_k,
         use_gpu=use_gpu,
     )
 
-    answer_parser = AnswerParser()  # pattern=r"\s+(.*)")
-    lfqa_prompt = PromptTemplate(
-        prompt="Answer the question using the provided context.\n\nContext: {join(documents)}\n\nQuestion: {query}\n\nAnswer (a few words or a phrase):",
-        output_parser=answer_parser,
-    )
     prompter = PromptNode(
         model_name_or_path="MBZUAI/LaMini-Flan-T5-783M",
-        default_prompt_template=lfqa_prompt,
+        default_prompt_template=default_prompt,
         model_kwargs={"model_max_length": 2048, "torch_dtype": torch.bfloat16},
         use_gpu=use_gpu,
     )
@@ -85,25 +83,20 @@ def embed_meta(documents: List[Document], use_gpu: bool = False) -> Pipeline:
     return p
 
 
-def squad(documents: List[Document], use_gpu: bool = False) -> Pipeline:
+def squad(documents: List[Document], use_gpu: bool = False, top_k: int = 1) -> Pipeline:
     document_store = InMemoryDocumentStore(use_gpu=use_gpu, use_bm25=True)
     document_store.write_documents(documents)
     retriever = BM25Retriever(document_store=document_store, top_k=100)
 
     reranker = SentenceTransformersRanker(
         model_name_or_path="cross-encoder/qnli-distilroberta-base",
-        top_k=1,
+        top_k=top_k,
         use_gpu=use_gpu,
     )
 
-    answer_parser = AnswerParser()  # pattern=r"\s+(.*)")
-    lfqa_prompt = PromptTemplate(
-        prompt="Answer the question using the provided context.\n\nContext: {join(documents)}\n\nQuestion: {query}\n\nAnswer (a few words or a phrase):",
-        output_parser=answer_parser,
-    )
     prompter = PromptNode(
         model_name_or_path="MBZUAI/LaMini-Flan-T5-783M",
-        default_prompt_template=lfqa_prompt,
+        default_prompt_template=default_prompt,
         model_kwargs={"model_max_length": 2048, "torch_dtype": torch.bfloat16},
         use_gpu=use_gpu,
     )
@@ -115,127 +108,41 @@ def squad(documents: List[Document], use_gpu: bool = False) -> Pipeline:
     return p
 
 
-def llama_old(documents: List[Document], use_gpu: bool = False) -> Pipeline:
+def few_shot_pipeline(
+    questions: List[str],
+    answers: List[str],
+    n_examples: int = 1,
+    use_gpu: bool = False,
+) -> Pipeline:
+    docs = []
+    for q, a in zip(questions, answers):
+        docs.append(Document(content=q, meta={"answer": a}))
+
+    document_store = InMemoryDocumentStore(use_gpu=use_gpu, use_bm25=True)
+    document_store.write_documents(docs)
+    retriever = BM25Retriever(document_store=document_store, top_k=n_examples)
+
+    p = Pipeline()
+    p.add_node(component=retriever, name="Retriever", inputs=["Query"])
+    return p
+
+
+def llama(documents: List[Document], use_gpu=False, top_k: int = 1) -> Pipeline:
     document_store = InMemoryDocumentStore(use_gpu=use_gpu, use_bm25=True)
     document_store.write_documents(documents)
     retriever = BM25Retriever(document_store=document_store, top_k=100)
 
     reranker = SentenceTransformersRanker(
         model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2",
-        top_k=1,
+        top_k=top_k,
         use_gpu=use_gpu,
     )
 
-    answer_parser = AnswerParser()  # pattern=r"\s+(.*)")
-    lfqa_prompt = PromptTemplate(
-        prompt="""Answer the question using the provided context.
-Example:
-Question: Who is teaching 11-711 in Spring 2024?
-
-Answer: Neubig.
-
-Context: {join(documents)}
-
-Question: {query}
-
-Answer: """,
-        output_parser=answer_parser,
-    )
     prompter = PromptNode(
-        model_name_or_path="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-        default_prompt_template=lfqa_prompt,
-        model_kwargs={"model_max_length": 2048, "torch_dtype": torch.bfloat16},
-        use_gpu=True
+        model_name_or_path="gpt-3.5-turbo",
+        default_prompt_template=default_prompt,
+        api_key=os.environ.get("OPENAI_API_KEY"),
     )
-
-    p = Pipeline()
-    p.add_node(component=retriever, name="Retriever", inputs=["Query"])
-    p.add_node(component=reranker, name="Reranker", inputs=["Retriever"])
-    p.add_node(component=prompter, name="prompt_node", inputs=["Reranker"])
-    return p
-
-
-def llama(documents: List[Document], use_gpu=False) -> Pipeline:
-    document_store = InMemoryDocumentStore(use_gpu=use_gpu, use_bm25=True)
-    document_store.write_documents(documents)
-    retriever = BM25Retriever(document_store=document_store, top_k=100)
-
-    reranker = SentenceTransformersRanker(
-        model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2", top_k=1, use_gpu=use_gpu
-    )
-
-    answer_parser = AnswerParser()  # pattern=r"\s+(.*)")
-    lfqa_prompt = PromptTemplate(
-        prompt="""Answer the question using the provided context. Please answer as concisely as possible.
-
-Context: {join(documents)}
-
-Question: {query}
-
-Answer: """,
-        output_parser=answer_parser,
-    )
-    prompter = PromptNode(model_name_or_path="gpt-3.5-turbo", default_prompt_template=lfqa_prompt, api_key=os.environ.get("OPENAI_API_KEY"))
-
-    p = Pipeline()
-    p.add_node(component=retriever, name="Retriever", inputs=["Query"])
-    p.add_node(component=reranker, name="Reranker", inputs=["Retriever"])
-    p.add_node(component=prompter, name="Prompter", inputs=["Reranker"])
-    return p
-
-def llama_few_k2(documents: List[Document], use_gpu=False) -> Pipeline:
-    document_store = InMemoryDocumentStore(use_gpu=use_gpu, use_bm25=True)
-    document_store.write_documents(documents)
-    retriever = BM25Retriever(document_store=document_store, top_k=100)
-
-    reranker = SentenceTransformersRanker(
-        model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2", top_k=2, use_gpu=use_gpu
-    )
-
-    answer_parser = AnswerParser()  # pattern=r"\s+(.*)")
-    lfqa_prompt = PromptTemplate(
-        prompt="""Answer the question using the provided context. Please answer as concisely as possible.
-For example:
-Question: Who is teaching 11-711 in Spring 2024?
-Answer: Neubig.
-
-
-Context: {join(documents)}
-Question: {query}
-Answer: """,
-        output_parser=answer_parser,
-    )
-    prompter = PromptNode(model_name_or_path="gpt-3.5-turbo", default_prompt_template=lfqa_prompt, api_key=os.environ.get("OPENAI_API_KEY"))
-
-    p = Pipeline()
-    p.add_node(component=retriever, name="Retriever", inputs=["Query"])
-    p.add_node(component=reranker, name="Reranker", inputs=["Retriever"])
-    p.add_node(component=prompter, name="Prompter", inputs=["Reranker"])
-    return p
-
-def llama_few(documents: List[Document], use_gpu=False) -> Pipeline:
-    document_store = InMemoryDocumentStore(use_gpu=use_gpu, use_bm25=True)
-    document_store.write_documents(documents)
-    retriever = BM25Retriever(document_store=document_store, top_k=100)
-
-    reranker = SentenceTransformersRanker(
-        model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2", top_k=1, use_gpu=use_gpu
-    )
-
-    answer_parser = AnswerParser()  # pattern=r"\s+(.*)")
-    lfqa_prompt = PromptTemplate(
-        prompt="""Answer the question using the provided context. Please answer as concisely as possible.
-For example:
-Question: Who is teaching 11-711 in Spring 2024?
-Answer: Neubig.
-
-
-Context: {join(documents)}
-Question: {query}
-Answer: """,
-        output_parser=answer_parser,
-    )
-    prompter = PromptNode(model_name_or_path="gpt-3.5-turbo", default_prompt_template=lfqa_prompt, api_key=os.environ.get("OPENAI_API_KEY"))
 
     p = Pipeline()
     p.add_node(component=retriever, name="Retriever", inputs=["Query"])
